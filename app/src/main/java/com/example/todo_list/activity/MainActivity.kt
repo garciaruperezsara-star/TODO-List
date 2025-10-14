7a5f826ab4b53d21a55ba9dc99d31f3631741a0d
Lavebara <garciaruperez.sara@gmail.com>
2025-10-14 13:51:52 +0200

    db implemented, todo tasks, recycler in progres

--- app/src/main/java/com/example/todo_list/activity/MainActivity.kt
+++ app/src/main/java/com/example/todo_list/activity/MainActivity.kt
@@ -18,8 +18,8 @@ class MainActivity : AppCompatActivity() {
         var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
