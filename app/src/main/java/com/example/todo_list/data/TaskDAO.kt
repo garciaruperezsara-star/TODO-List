62a40d5ed48f594c432cdc74eddb967bf2860e78
Lavebara <garciaruperez.sara@gmail.com>
2025-10-15 13:49:04 +0200

    Task in DB almost implemented

--- app/src/main/java/com/example/todo_list/data/TaskDAO.kt
+++ app/src/main/java/com/example/todo_list/data/TaskDAO.kt
@@ -1,5 +1,6 @@
 package com.example.todo_list.data
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.util.Log
@@ -14,6 +15,101 @@ class TaskDAO(val context: Context) {
 
     private fun close() {
         db.close()
+    }
+
+    fun insert(task: Task, task_id: String) {
+        // Create a new map of values, where column names are the keys
+        val values = ContentValues()
+        values.put(Task.COLUMN_TITLE, task.title)
+        values.put(Task.COLUMN_DONE, false)
+        values.put(Task.COLUMN_CATEGORY, task_id)
+        try {
+            open()
+            // Insert the new row, returning the primary key value of the new row
+            val newRowId = db.insert(Task.TABLE_NAME, null, values)
+            Log.i("DATABASE", "New row inserted in table ${Task.TABLE_NAME} with id: $newRowId")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+    }
+
+    fun update(task: Task, done: Boolean, task_id: String) {
+        // Create a new map of values, where column names are the keys
+        val values = ContentValues()
+        values.put(Task.COLUMN_TITLE, task.title)
+        values.put(Task.COLUMN_DONE, false)
+        values.put(Task.COLUMN_CATEGORY, task_id)
+
+        try {
+            open()
+
+            // Insert the new row, returning the primary key value of the new row
+            val updatedRows = db.update(Task.TABLE_NAME, values, "${Task.COLUMN_ID} = ${task.id}", null)
+            Log.i("DATABASE", "$updatedRows rows updated in table ${Task.TABLE_NAME}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+    }
+
+    fun delete(id: Int) {
+        try {
+            open()
+
+            // Insert the new row, returning the primary key value of the new row
+            val deletedRows = db.delete(Task.TABLE_NAME, "${Task.COLUMN_ID} = $id", null)
+            Log.i("DATABASE", "$deletedRows rows deleted in table ${Task.TABLE_NAME}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+    }
+
+    fun find(id: Int) : Task? {
+        var task: Task? = null
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        val projection = arrayOf(Task.COLUMN_ID, Task.COLUMN_TITLE, Task.COLUMN_DONE, Task.COLUMN_CATEGORY)
+
+        // Filter results WHERE "title" = 'My Title'
+        val selection = "${Task.COLUMN_ID} = $id"
+        val selectionArgs = null
+
+        // How you want the results sorted in the resulting Cursor
+        val sortOrder = null
+
+        try {
+            open()
+            val cursor = db.query(
+                Task.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+            )
+
+            // Read the cursor data
+            if (cursor.moveToNext()) {
+                val id = cursor.getInt(cursor.getColumnIndexOrThrow(Task.COLUMN_ID))
+                val title = cursor.getString(cursor.getColumnIndexOrThrow(Task.COLUMN_TITLE))
+                val done = cursor.getInt(cursor.getColumnIndexOrThrow(Task.COLUMN_DONE))!=0
+                val categoryId = cursor.getInt(cursor.getColumnIndexOrThrow(Task.COLUMN_CATEGORY))
+                task = Task(id, title, done, categoryId)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+
+        return task
     }
 
 }
--- app/src/main/java/com/example/todo_list/utils/DataBaseManager.kt
+++ app/src/main/java/com/example/todo_list/utils/DataBaseManager.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.todo_list.data.Category
+import com.example.todo_list.data.Task
 
-class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "toDoList.db", null, 1) {
+class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "toDoList.db", null, 2) {
 
     override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("PRAGMA foreign_keys= ON")
         db.execSQL(Category.SQL_CREATE_TABLE)
+        db.execSQL(Task.SQL_CREATE_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
@@ -17,6 +20,7 @@ class DatabaseManager(context: Context) : SQLiteOpenHelper(context, "toDoList.db
     }
 
     fun onDestroy(db: SQLiteDatabase) {
+        db.execSQL(Task.SQL_DROP_TABLE)
         db.execSQL(Category.SQL_DROP_TABLE)
     }
 }
